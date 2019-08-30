b24cb89e37dc2f4aa4efcbd8356d1f92fc92e94f
Zimmee <zimmee@ross.localdomain>
2019-08-30 14:33:05 -0600

    fix(hammerspoon): Fix hammerspoon config.

--- hammerspoon/hammerspoon.link/init.lua
+++ hammerspoon/hammerspoon.link/init.lua
@@ -1,8 +1,26 @@
-local HYPER = {
-	key = {'ctrl', 'alt'},
-	cmd = {'ctrl', 'alt', 'cmd'},
-	shift = {'ctrl', 'alt', 'shift'},
-	cmd_shift = {'ctrl', 'alt', 'shift', 'cmd'},
-}
-local WindowGrid = hs.loadSpoon('WindowGrid')
-WindowGrid:bindHotkeys(hs.hotkey.new(HYPER.key, 'o'))
+-- local WindowGrid = hs.loadSpoon('WindowGrid')
+-- WindowGrid:bindHotkeys(hs.hotkey.new('ctrl', 'o'))
+-- WindowGrid:start()
+
+-- [[ START ]] Setup.
+local config = require 'config'
+local grid = require 'grid'
+-- [[ END ]]
+
+
+-- [[ START ]] Window grid.
+grid.destroy()
+grid.init()
+-- [[ END ]]
+
+
+-- [[ START ]] Reload config.
+hs.hotkey.bind(config.hyper_cmd_shift, '\\', function()
+  hs.reload()
+end)
+-- [[ END ]]
+
+
+-- [[ START ]] Show reload config message.
+hs.alert.show('Config reloaded!')
+-- [[ END ]]
