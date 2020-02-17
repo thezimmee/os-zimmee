9c905db12c4b54e12f1627e0490c48acc0833199
Zimmee <zimmee@betsy.localdomain>
2020-02-17 15:21:31 -0700

    fix(sublime): Fix JS linting with standard due to issue with NVM.

--- node/node.zsh
+++ node/node.zsh
@@ -7,6 +7,7 @@
 export NVM_DIR="$HOME/.nvm"
   [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
   [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+NVM_SYMLINK_CURRENT=true
 
 alias nr='npm run'
 alias tags='npm dist-tag ls'
