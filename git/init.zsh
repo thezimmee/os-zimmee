14c26ed97cf2545a125b9a8cb7b03f87a6260142
Tyson Zimmerman <thezimmee@gmail.com>
2023-07-19 15:27:59 -0600

    Run ssh agent on startup.

--- git/init.zsh
+++ git/init.zsh
@@ -6,15 +6,9 @@
 alias gs='git status'
 
 # Initialize git completions (installed with zsh).
-# source /usr/local/share/zsh/site-functions/git-completion.zsh
-
-# @todo: Explore using `hub` as our git wrapper (http://defunkt.github.com/hub/).
-# hub_path=$(which hub)
-# if (( $+commands[hub] ))
-# then
-#   alias git=$hub_path
-# fi
+source /usr/local/share/zsh/site-functions/git-completion.zsh
 
 # Run ssh-agent.
-# eval "$(ssh-agent -s)" && ssh-add -K ~/.ssh/id_rsa
-# plugins=(git ssh-agent)
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_rsa
+ssh-add ~/.ssh/id_ed25519
